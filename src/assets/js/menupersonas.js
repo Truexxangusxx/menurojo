38ce59d087ed084ba5bcecd7cebe4bc3388f3cb2
Carlos Edgardo Medina Carranza <cmedinac@pe.ibm.com>
2018-02-01 08:56:02 -0500

    terminado menu

--- src/assets/js/menupersonas.js
+++ src/assets/js/menupersonas.js
@@ -11,28 +11,38 @@ $(document).ready(function () {
         $(".tercer-menu").removeClass('right');
         $(".tercer-menu").removeClass('left');
         $(".segundo-menu").removeClass('inright');
-        
+
         if ($(".contenido-menu").hasClass('on') || $(".contenido-menu").hasClass('off')) {
             $(".contenido-menu").toggleClass('on');
             $(".contenido-menu").toggleClass('off');
             $(".bloque-opacity").toggleClass('on-opacity');
-            
+            if ($(".id135.slick-initialized").length > 0) {
+                if ($(".bloque-opacity").hasClass('on-opacity')) {
+                    $(".id135.slick-initialized").slick('pause');
+                }
+                else {
+                    $(".id135.slick-initialized").slick('play');
+                }
+            }
         }
         else {
             $(".contenido-menu").addClass('on');
             $(".bloque-opacity").addClass('on-opacity');
+            if ($(".id135.slick-initialized").length > 0) {
+                $(".id135.slick-initialized").slick('pause');
+            }
         }
     });
 
-    $(".ir-izquierda").click(function(e){
+    $(".ir-izquierda").click(function (e) {
         e.preventDefault();
         $(".contenido-menu").addClass('left');
-        $(".segundo-menu."+$(this).attr("menu")).addClass('left');
+        $(".segundo-menu." + $(this).attr("menu")).addClass('left');
         $(".contenido-menu").removeClass('right');
         $(".segundo-menu").removeClass('right');
     });
-    
-    $(".regresar-derecha").click(function(e){
+
+    $(".regresar-derecha").click(function (e) {
         e.preventDefault();
         $(".segundo-menu").removeClass('inright');
         $(".contenido-menu").removeClass('left');
@@ -41,19 +51,19 @@ $(document).ready(function () {
         $(this).parent().addClass('right');
     });
 
-    $(".ir-tercero").click(function(e){
+    $(".ir-tercero").click(function (e) {
         e.preventDefault();
         $(".segundo-menu").removeClass('inright');
         $(".segundo-menu").addClass('outleft');
-        $(".tercer-menu."+$(this).attr("menu")).removeClass('right');
-        $(".tercer-menu."+$(this).attr("menu")).addClass('left');
+        $(".tercer-menu." + $(this).attr("menu")).removeClass('right');
+        $(".tercer-menu." + $(this).attr("menu")).addClass('left');
     });
 
-    $(".regresar-tercero").click(function(e){
+    $(".regresar-tercero").click(function (e) {
         e.preventDefault();
         $(".segundo-menu").removeClass('outleft');
         $(this).parent().removeClass('left');
-        $(".segundo-menu."+$(this).attr("menu")).addClass('inright');
+        $(".segundo-menu." + $(this).attr("menu")).addClass('inright');
         $(this).parent().addClass('right');
     });
 
